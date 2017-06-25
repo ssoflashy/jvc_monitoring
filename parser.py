d6dbdf9d4412ba2c95671038d43361605085a55a
ssoflashy <rdevannes88@gmail.com>
2017-06-25 17:54:49 +0200

    Add files via upload

--- parser.py
+++ parser.py
@@ -5,46 +5,62 @@ from urllib.request import urlopen
 from time import sleep
 import sqlite3
 import datetime
+import requests
+import logging
+import re
 
 
 
-class Forum():
+class Forum:
 
 	def __init__(self, forum, url_forum):
 		self.forum = forum
 		self.url_forum = url_forum
 
+		pattern = '([0-9]{1,5})'
+		self.pattern = re.compile(pattern)
+
 	def add_to_database(self):
 		connection = sqlite3.connect("database.db")
 		c = connection.cursor()
-		self.now = datetime.datetime.today()
-		self.text = "INSERT INTO {0}(datetime, nb_co) VALUES('{1}', '{2}')".format(self.forum, self.now, self.recup_co())
+		now = datetime.datetime.today()
+		text = "INSERT INTO {0}(datetime, nb_co) VALUES('{1}', '{2}')".format(self.forum, now, self.recup_co())
+		print(text)
 		print(self.forum, self.recup_co())
-		c.execute(self.text)
+		c.execute(text)
 		connection.commit()
 		connection.close()
-		sleep(5)
+		sleep(1)
 
 	def recup_co(self):
-		self.page_html = str(urlopen(self.url_forum).read()) # stockage du contenu de la page dans une variable
+		r = requests.get(self.url_forum)
+		page_html = str(r.text) # stockage du contenu de la page dans une variable
+		page = BeautifulSoup(page_html, 'html.parser') 
+		resultat = page.select(".nb-connect-fofo")
 
-		self.page = BeautifulSoup(self.page_html, 'html.parser') 
-		self.resultat = self.page.select(".nb-connect-fofo")
+		print(resultat)
+		temp = str(resultat) #stockage de la ligne avec le nombre de co
 
-		self.nb_co = str(self.resultat) #stockage de la ligne avec le nombre de co
+		nb_co = re.search(self.pattern, temp)
 
-		self.nb_co = self.nb_co[:-27] #modification de la ligne pour ne garder que le nb de co
-		self.nb_co = self.nb_co[31:]
-		
-		return self.nb_co
+		print(nb_co[0])
 
-dixhuit_vingtcinq = Forum("dixhuit_vingtcinq", "http://www.jeuxvideo.com/forums/0-51-0-1-0-1-0-blabla-18-25-ans.htm")
-moins_quinze = Forum("moins_quinze", "http://www.jeuxvideo.com/forums/0-15-0-1-0-1-0-blabla-moins-de-15-ans.htm")
-quinze_dixhuit = Forum("quinze_dixhuit", "http://www.jeuxvideo.com/forums/0-50-0-1-0-1-0-blabla-15-18-ans.htm")
-overwatch = Forum("overwatch", "http://www.jeuxvideo.com/forums/0-33972-0-1-0-1-0-overwatch.htm")
-while(True):
-	dixhuit_vingtcinq.add_to_database()
-	moins_quinze.add_to_database()
-	quinze_dixhuit.add_to_database()
-	overwatch.add_to_database()
-	sleep(60)
+		return nb_co[0]
+
+def main():
+	logging.basicConfig(filename="logs.log", level=logging.ERROR)
+	dixhuit_vingtcinq = Forum("dixhuit_vingtcinq", "http://www.jeuxvideo.com/forums/0-51-0-1-0-1-0-blabla-18-25-ans.htm")
+	moins_quinze = Forum("moins_quinze", "http://www.jeuxvideo.com/forums/0-15-0-1-0-1-0-blabla-moins-de-15-ans.htm")
+	quinze_dixhuit = Forum("quinze_dixhuit", "http://www.jeuxvideo.com/forums/0-50-0-1-0-1-0-blabla-15-18-ans.htm")
+	overwatch = Forum("overwatch", "http://www.jeuxvideo.com/forums/0-33972-0-1-0-1-0-overwatch.htm")
+	forums = [dixhuit_vingtcinq, moins_quinze, quinze_dixhuit, overwatch]
+	while(True):
+		for forum in forums:
+			try:
+				forum.add_to_database()
+			except:
+				print("Une erreur est arrivee pour le forum '{}'".format(forum.forum))
+				sleep(5)
+		sleep(30)
+
+main()
