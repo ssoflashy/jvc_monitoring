724f8a0892ae9211a747676c37a24e5f877d4dcd
ssoflashy <rdevannes88@gmail.com>
2017-07-02 21:47:07 +0200

    Add files via upload

--- parser.py
+++ parser.py
@@ -5,7 +5,6 @@ from time import sleep
 import sqlite3
 import datetime
 import requests
-import logging
 import re
 
 
@@ -23,39 +22,41 @@ class Forum:
 		connection = sqlite3.connect("database.db")
 		c = connection.cursor()
 		now = datetime.datetime.today()
-		text = "INSERT INTO {0}(datetime, nb_co) VALUES('{1}', '{2}')".format(self.forum, now, self.recup_co())
-		print(now, self.forum, self.recup_co())
+		nb_co = self.recup_co()
+
+		text = "INSERT INTO {0}(datetime, nb_co) VALUES('{1}', '{2}')".format(self.forum, now, nb_co)
 		c.execute(text)
 		connection.commit()
 		connection.close()
+
+		print(now, self.forum, str(nb_co))
 		sleep(1)
 
 	def recup_co(self):
 		r = requests.get(self.url_forum)
-		page_html = str(r.text) # stockage du contenu de la page dans une variable
+		page_html = str(r.text)
 		page = BeautifulSoup(page_html, 'html.parser') 
 		resultat = page.select(".nb-connect-fofo")
 
-		temp = str(resultat) #stockage de la ligne avec le nombre de co
-
-		nb_co = re.search(self.pattern, temp)
+		nb_co = re.search(self.pattern, str(resultat))
 
 		return nb_co.group(0)
 
 def main():
-	logging.basicConfig(filename="logs.log", level=logging.ERROR)
 	dixhuit_vingtcinq = Forum("dixhuit_vingtcinq", "http://www.jeuxvideo.com/forums/0-51-0-1-0-1-0-blabla-18-25-ans.htm")
 	moins_quinze = Forum("moins_quinze", "http://www.jeuxvideo.com/forums/0-15-0-1-0-1-0-blabla-moins-de-15-ans.htm")
 	quinze_dixhuit = Forum("quinze_dixhuit", "http://www.jeuxvideo.com/forums/0-50-0-1-0-1-0-blabla-15-18-ans.htm")
 	overwatch = Forum("overwatch", "http://www.jeuxvideo.com/forums/0-33972-0-1-0-1-0-overwatch.htm")
+
 	forums = [dixhuit_vingtcinq, moins_quinze, quinze_dixhuit, overwatch]
+
 	while(True):
 		for forum in forums:
 			try:
 				forum.add_to_database()
 			except:
-				print("Une erreur est arrivee pour le forum '{}'".format(forum.forum))
+				print("An error occured with the forum '{0}' at {1}".format(forum.forum, datetime.datetime.today()))
 				sleep(5)
-		sleep(30)
+		sleep(60)
 
 main()
